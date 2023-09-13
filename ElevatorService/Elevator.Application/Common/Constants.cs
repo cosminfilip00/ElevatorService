8e8b5e2b9f3b332510698ca6d9fcba85adbe8b57
Cosmin Filip <cosminfilip@outlook.com>
2023-09-13 17:32:40 +0300

    Change floor max value

--- ElevatorService/Elevator.Application/Common/Constants.cs
+++ ElevatorService/Elevator.Application/Common/Constants.cs
@@ -5,5 +5,5 @@ public static class Constants
     public const int PASSENGER_MIN_NUMBER = 1;
     public const int PASSENGER_MAX_NUMBER = 20;
     public const int FLOOR_MIN_NUMBER = 0;
-    public const int FLOOR_MAX_NUMBER = 20;
+    public const int FLOOR_MAX_NUMBER = 100;
 }
